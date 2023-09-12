ed770306ffcaee602234ca0a4a371320f1c6f3e3
Adeola Samuel <meetprecious1403@gmail.com>
2023-09-12 11:02:35 +0100

    chore: added file and repo url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
-export const GITHUB_FILE_URL = "";
+export const GITHUB_FILE_URL =
+  "https://github.com/a-deola/api-app/blob/main/src/index.ts";
 
-export const GITHUB_REPO_URL = "";
+export const GITHUB_REPO_URL = "https://github.com/a-deola/api-app";
